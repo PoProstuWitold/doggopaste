4f682944986c71f27bd609898c53149a8cb0f429
PoProstuWitold <witekzawada@o2.pl>
2025-11-12 16:48:10 +0100

    feat: custom toast styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Toaster } from 'react-hot-toast'
+import { FiCheckCircle, FiXCircle } from 'react-icons/fi'
 import { Container } from './components/core/Container'
 import { Footer } from './components/core/Footer'
 import { Navbar } from './components/core/NavBar'
@@ -24,7 +25,22 @@ export default function RootLayout({
 					<Navbar />
 					<Container>{children}</Container>
 					<Footer />
-					<Toaster />
+					<Toaster
+						toastOptions={{
+							className:
+								'!tracking-wider !font-semibold !bg-base-300 !backdrop-blur !text-base-content',
+							success: {
+								icon: (
+									<FiCheckCircle className='!text-success w-6 h-6' />
+								)
+							},
+							error: {
+								icon: (
+									<FiXCircle className='!text-error w-6 h-6' />
+								)
+							}
+						}}
+					/>
 				</ThemeProvider>
 			</body>
 		</html>
